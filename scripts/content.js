f8b04a28bb072757c35e92617e529d05198eacec
kicchom <kikkawa@goga.co.jp>
2025-01-09 02:20:53 +0900

    ポップアップのレイアウトと中身実装

--- scripts/content.js
+++ scripts/content.js
@@ -63,19 +63,43 @@ async function openAnnotationPopup(user_id) {
         <h3><img src="${icon_url}">X Followees Scribble</h3>
         <button class="close-btn">×</button>
       </div>
-      <textarea id="annotation-text" placeholder="Enter your notes here..."></textarea>
-      <button id="save-annotation">Save</button>
+      <div class="annotation-body">
+        <div class="user-info">
+          <img src="${user.latest.profile_image_url}" alt="User Image">
+          <div>
+            <p class="id">@${user.id}</p>
+            <a class="resync-id-btn">→ ユーザ ID が変わったかも？</a>
+            <p class="name">ユーザ名: ${user.latest.name}</p>
+          </div>
+        </div>
+        <div class="annotation">
+          <div class="memo">
+            <textarea id="annotation-text" placeholder="ここはメモ欄です。フォローしたきっかけや、過去にどんな絡みをしたかなど、好きに記述してみてください。"></textarea>
+          </div>
+          <div class="tags">
+            <input id="annotation-tags" type="text" placeholder="タグをつける">
+          </div>
+        </div>
+
+        <a class="history-btn">過去の情報を見る</a>
+
+      </div>
     </div>
   `;
 
   // 既存のアノテーションをロード
   popup.querySelector('#annotation-text').value = user.memo;
+  popup.querySelector('#annotation-tags').value = user.tags;
 
-  // 保存ボタンのイベントリスナー
-  popup.querySelector('#save-annotation').addEventListener('click', () => {
-    const text = popup.querySelector('#annotation-text').value;
-    Storage.setUser({...user, memo: text});
-  });
+  // 編集時のイベントリスナー
+  const save = () => {
+    // TODO: 保存した旨のメッセージを表示（差分確認する？）
+    const memo = popup.querySelector('#annotation-text').value;
+    const tags = popup.querySelector('#annotation-tags').value;
+    Storage.setUser({...user, memo: memo,tags: tags});
+  }
+  popup.querySelector('#annotation-text').addEventListener('blur', save);
+  popup.querySelector('#annotation-tags').addEventListener('blur', save);
 
   // 閉じるボタンのイベントリスナー
   popup.querySelector('.close-btn').addEventListener('click', () => {
